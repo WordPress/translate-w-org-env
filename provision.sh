2af6eb3eef58ac543a3d47b374eba0f75cda5a24
Jesús Amieiro <amieiro@gmail.com>
2021-12-16 20:48:12 +0100

    Update some commands to adapt them to the LAMP installation

--- provision.sh
+++ provision.sh
@@ -131,10 +131,22 @@ function clone_repos() {
 function copy_repos() {
   # Do the same that the mappings in the .wp-env.json file
   echo "${YELLOW}Coping some items from the repos to WordPress.${RESET}"
-  cp -R ./meta.git/wordpress.org/public_html/wp-content $1/wp-content
-  cp -R ./glotpress.git $1/wp-content/plugins/glotpress
+  cp -f -R ./meta.git/wordpress.org/public_html/wp-content/mu-plugins $1/wp-content/
+  cp -f -R ./meta.git/wordpress.org/public_html/wp-content/plugins $1/wp-content/
+  cp -f -R ./meta.git/wordpress.org/public_html/wp-content/themes $1/wp-content/
+  cp -f -R ./meta.git/wordpress.org/public_html/wp-content/upgrade $1/wp-content/
+  cp -f -R ./meta.git/wordpress.org/public_html/wp-content/uploads $1/wp-content/
+  cp -f -R ./glotpress.git $1/wp-content/plugins/glotpress
+  cp -f -R ./meta.git/global.wordpress.org/public_html/wp-content/themes/rosetta $1/wp-content/themes/
   cp ./.wp-env/.htaccess $1/
   echo "${GREEN}Items copied.${RESET}"
+}
+
+function set_environment_variables() {
+  wp config set WP_ENVIRONMENT_TYPE local --path=$1
+  wp config set WPORGPATH $1/wp-content/themes/pub/wporg/inc/ --path=$1
+  wp config set GP_URL_BASE '/' --path=$1
+  wp config set GP_TMPL_PATH $1/wp-content/plugins/wporg-gp-customizations/templates/ --path=$1
 }
 
 # Todo: add in the meta.git .gitignore file all the files that have been copied to it
@@ -175,6 +187,7 @@ if [ "$TYPE" == "lamp" ]; then
   install_wordpress $PROJECT_PATH $LOCAL_URL
   clone_repos
   copy_repos $PROJECT_PATH
+  set_environment_variables $PROJECT_PATH
   WP_CLI_PREFIX=""
   WP_CLI_SUFFIX="--path=$PROJECT_PATH"
 fi
@@ -199,91 +212,111 @@ fi
 # Enable the rosetta theme
 # To see the available themes, execute: npm run wp-env run cli wp theme list
 print_header "Enabling the rosetta theme"
-$WP_CLI_PREFIX wp theme activate rosetta
+$WP_CLI_PREFIX wp theme activate rosetta $WP_CLI_SUFFIX
 print_header "Importing the translation tables"
 $WP_CLI_PREFIX wp db import tmp/translate_tables.sql $WP_CLI_SUFFIX
 # Remove this table, because the old dump hasn't some fields
-$WP_CLI_PREFIX wp db query '""DROP TABLE translate_project_translation_status""' $WP_CLI_SUFFIX
+if [ "$TYPE" == "lamp" ]; then
+  $WP_CLI_PREFIX wp db query 'DROP TABLE translate_project_translation_status' $WP_CLI_SUFFIX
+else
+  $WP_CLI_PREFIX wp db query '""DROP TABLE translate_project_translation_status""' $WP_CLI_SUFFIX
+fi
 $WP_CLI_PREFIX wp db import tmp/translate_project_translation_status.sql $WP_CLI_SUFFIX
 
 print_header "Updating the database prefix for GlotPress as variable in the wp-config.php"
-npm run wp-env run cli wp config set gp_table_prefix translate_ \"--type=variable\"
+if [ "$TYPE" == "lamp" ]; then
+  $WP_CLI_PREFIX wp config set gp_table_prefix translate_ --type=variable $WP_CLI_SUFFIX
+else
+  $WP_CLI_PREFIX wp config set gp_table_prefix translate_ \"--type=variable\" $WP_CLI_SUFFIX
+fi
 
 print_header "Updating the site name"
-npm run wp-env run cli wp option update blogname \'translate.wordpress.local\'
-npm run wp-env run cli wp option update blogdescription \'WordPress.org translation system\'
+if [ "$TYPE" == "lamp" ]; then
+  $WP_CLI_PREFIX wp option update blogname translate.wordpress.local $WP_CLI_SUFFIX
+  $WP_CLI_PREFIX wp option update blogdescription 'WordPress.org translation system' $WP_CLI_SUFFIX
+else
+  $WP_CLI_PREFIX wp option update blogname \'translate.wordpress.local\' $WP_CLI_SUFFIX
+  $WP_CLI_PREFIX wp option update blogdescription \'WordPress.org translation system\' $WP_CLI_SUFFIX
+fi
 
 ## Todo: try to update this on the source code
 print_header "Updating some files that have been modified for the local environment"
-cp tmp/class-index.php ./meta.git/wordpress.org/public_html/wp-content/plugins/wporg-gp-routes/inc/routes/class-index.php
-cp tmp/functions.php meta.git/wordpress.org/public_html/wp-content/themes/pub/wporg/functions.php
-cp tmp/header.php meta.git/wordpress.org/public_html/wp-content/themes/pub/wporg/inc/header.php
+if [ "$TYPE" == "lamp" ]; then
+  cp -v tmp/class-index.php $PROJECT_PATH/wp-content/plugins/wporg-gp-routes/inc/routes/class-index.php
+  cp -v tmp/functions.php $PROJECT_PATH/wp-content/themes/pub/wporg/functions.php
+  cp -v tmp/header.php $PROJECT_PATH/wp-content/themes/pub/wporg/inc/header.php
+else
+  cp tmp/class-index.php ./meta.git/wordpress.org/public_html/wp-content/plugins/wporg-gp-routes/inc/routes/class-index.php
+  cp tmp/functions.php meta.git/wordpress.org/public_html/wp-content/themes/pub/wporg/functions.php
+  cp tmp/header.php meta.git/wordpress.org/public_html/wp-content/themes/pub/wporg/inc/header.php
+fi
 
 print_header "Creating some new users"
-npm run wp-env run cli wp  user create translator01 translator01@example.com '"--user_pass=password"' '"--role=subscriber"'
-npm run wp-env run cli wp  user create translator02 translator02@example.com '"--user_pass=password"' '"--role=subscriber"'
-npm run wp-env run cli wp  user create translator03 translator03@example.com '"--user_pass=password"' '"--role=subscriber"'
-npm run wp-env run cli wp  user create translator04 translator04@example.com '"--user_pass=password"' '"--role=subscriber"'
-npm run wp-env run cli wp  user create translator05 translator05@example.com '"--user_pass=password"' '"--role=subscriber"'
+if [ "$TYPE" == "lamp" ]; then
+  $WP_CLI_PREFIX wp user create translator01 translator01@example.com --user_pass=password --role=subscriber $WP_CLI_SUFFIX
+else
+  $WP_CLI_PREFIX wp user create translator01 translator01@example.com '"--user_pass=password"' '"--role=subscriber"' $WP_CLI_SUFFIX
+fi
 
 # See https://developer.wordpress.org/block-editor/reference-guides/packages/packages-env/#installing-a-plugin-or-theme-on-the-development-instance
 # This if executes the plugin installation with docker-compose on Linux systems and with npm on Mac
 # This only works if we have 1 folder inside ~/wp-env
 # Todo: get the ~/wp-env folder where is the WordPress Docker configuration if the user has more than one Docker installation
 print_header "Installing and activating some plugins"
-if [ -d "~/wp-env" ]
-then
+if [ "$TYPE" == "lamp" ]; then
+  $WP_CLI_PREFIX wp plugin install --activate ${WPCLI_PLUGINS[@]} $WP_CLI_SUFFIX
+elif [ -d "~/wp-env" ]; then
   WPENV_PATH=$(ls ~/wp-env -1 | head -1)
   cd ~/wp-env/$WPENV_PATH
   docker-compose run --rm -u $(id -u) -e HOME=/tmp cli plugin install --activate ${WPCLI_PLUGINS[@]}
   cd -
 else
-  npm run wp-env run cli wp plugin install '"--activate"' ${WPCLI_PLUGINS[@]}
+  $WP_CLI_PREFIX wp plugin install '"--activate"' ${WPCLI_PLUGINS[@]} $WP_CLI_SUFFIX
 fi
 
 # To see the GlotPress plugins, execute: npm run wp-env run cli wp plugin list | grep gp
 print_header "Activating GlotPress and its plugins"
-npm run wp-env run cli wp plugin activate glotpress wporg-gp-js-warnings wporg-gp-routes wporg-gp-custom-stats \
+$WP_CLI_PREFIX wp plugin activate glotpress wporg-gp-js-warnings wporg-gp-routes wporg-gp-custom-stats \
 wporg-gp-custom-warnings wporg-gp-help wporg-gp-plugin-directory wporg-gp-slack-integrations wporg-gp-theme-directory \
-wporg-gp-translation-fixer wporg-gp-translation-suggestions wporg-gp-customizations glotpress-translate-bridge
+wporg-gp-translation-fixer wporg-gp-translation-suggestions wporg-gp-customizations glotpress-translate-bridge $WP_CLI_SUFFIX
 
 print_header "Downloading and importing the WordPress strings"
 # Download the WordPress po file and import it
 curl -o tmp/po/wordpress-dev.po https://translate.wordpress.org/projects/wp/dev/gl/default/export-translations/?filters%5Bstatus%5D=current_or_waiting_or_fuzzy_or_untranslated
-npm run wp-env run cli wp glotpress import-originals '"wp/dev"' tmp/po/wordpress-dev.po
+$WP_CLI_PREFIX wp glotpress import-originals wp/dev tmp/po/wordpress-dev.po $WP_CLI_SUFFIX
 curl -o tmp/po/continents.po https://translate.wordpress.org/projects/wp/dev/cc/gl/default/export-translations/?filters%5Bstatus%5D=current_or_waiting_or_fuzzy_or_untranslated
-npm run wp-env run cli wp glotpress import-originals '"wp/dev/cc"' tmp/po/continents.po
+$WP_CLI_PREFIX wp glotpress import-originals wp/dev/cc tmp/po/continents.po $WP_CLI_SUFFIX
 curl -o tmp/po/admin.po https://translate.wordpress.org/projects/wp/dev/admin/gl/default/export-translations/?filters%5Bstatus%5D=current_or_waiting_or_fuzzy_or_untranslated
-npm run wp-env run cli wp glotpress import-originals '"wp/dev/admin"' tmp/po/admin.po
+$WP_CLI_PREFIX wp glotpress import-originals wp/dev/admin tmp/po/admin.po $WP_CLI_SUFFIX
 curl -o tmp/po/admin-network.po https://translate.wordpress.org/projects/wp/dev/admin/network/gl/default/export-translations/?filters%5Bstatus%5D=current_or_waiting_or_fuzzy_or_untranslated
-npm run wp-env run cli wp glotpress import-originals '"wp/dev/admin/network"' tmp/po/admin-network.po
+$WP_CLI_PREFIX wp glotpress import-originals wp/dev/admin/network tmp/po/admin-network.po $WP_CLI_SUFFIX
 
 print_header "Downloading and importing some plugin strings"
 # Some plugins or readme will fail, because it doesn't have the stable version
 for plugin in "${PLUGINS_TO_TRANSLATE[@]}"
 do :
   curl -o "tmp/po/${plugin}-dev.po" "https://translate.wordpress.org/projects/wp-plugins/${plugin}/dev/gl/default/export-translations/?filters%5Bstatus%5D=current_or_waiting_or_fuzzy_or_untranslated"
-  npm run wp-env run cli wp glotpress import-originals "wp-plugins/${plugin}/dev" "tmp/po/${plugin}-dev.po"
-  npm run wp-env run cli wp glotpress import-originals "wp-plugins/${plugin}/stable" "tmp/po/${plugin}-dev.po"
+  $WP_CLI_PREFIX wp glotpress import-originals wp-plugins/$plugin/dev tmp/po/$plugin-dev.po $WP_CLI_SUFFIX
+  $WP_CLI_PREFIX wp glotpress import-originals wp-plugins/$plugin/stable tmp/po/$plugin-dev.po $WP_CLI_SUFFIX
   curl -o "tmp/po/${plugin}-dev-readme.po" "https://translate.wordpress.org/projects/wp-plugins/${plugin}/dev-readme/gl/default/export-translations/?filters%5Bstatus%5D=current_or_waiting_or_fuzzy_or_untranslated"
-  npm run wp-env run cli wp glotpress import-originals "wp-plugins/${plugin}/dev-readme" "tmp/po/${plugin}-dev-readme.po"
-  npm run wp-env run cli wp glotpress import-originals "wp-plugins/${plugin}/stable-readme" "tmp/po/${plugin}-dev-readme.po"
+  $WP_CLI_PREFIX wp glotpress import-originals wp-plugins/$plugin/dev-readme tmp/po/$plugin-dev-readme.po $WP_CLI_SUFFIX
+  $WP_CLI_PREFIX wp glotpress import-originals wp-plugins/$plugin/stable-readme tmp/po/$plugin-dev-readme.po $WP_CLI_SUFFIX
 done
 
 print_header "Downloading and importing some meta strings"
 for element in "${META_TO_TRANSLATE[@]}"
 do :
   curl -o "tmp/po/${element}.po" "https://translate.wordpress.org/projects/meta/${element}/gl/default/export-translations/?filters%5Bstatus%5D=current_or_waiting_or_fuzzy_or_untranslated"
-  npm run wp-env run cli wp glotpress import-originals "meta/${element}" "tmp/po/${element}.po"
+  $WP_CLI_PREFIX wp glotpress import-originals meta/$element tmp/po/$element.po $WP_CLI_SUFFIX
 done
 
 print_header "Downloading and importing some apps strings"
 for element in "${APPS_TO_TRANSLATE[@]}"
 do :
   curl -o "tmp/po/${element}.po" "https://translate.wordpress.org/projects/apps/${element}/dev/es/default/export-translations/?filters%5Bstatus%5D=current_or_waiting_or_fuzzy_or_untranslated"
-  npm run wp-env run cli wp glotpress import-originals "apps/${element}/dev" "tmp/po/${element}.po"
+  $WP_CLI_PREFIX wp glotpress import-originals apps/$element/dev tmp/po/$element.po $WP_CLI_SUFFIX
   curl -o "tmp/po/${element}-release-notes.po" "https://translate.wordpress.org/projects/apps/${element}/release-notes/es/default/export-translations/?filters%5Bstatus%5D=current_or_waiting_or_fuzzy_or_untranslated"
-  npm run wp-env run cli wp glotpress import-originals "apps/${element}/release-notes" "tmp/po/${element}-release-notes.po"
+  $WP_CLI_PREFIX wp glotpress import-originals apps/$element/release-notes tmp/po/$element-release-notes.po $WP_CLI_SUFFIX
 done
 
 print_header "Removing the .po files"
@@ -291,8 +324,17 @@ rm tmp/po/*.po
 
 # Set the permalinks format, because GlotPress needs it
 print_header "Updating the rewrite structure"
-npm run wp-env run cli wp rewrite structure '"/%postname%/"' '"--hard"'
-npm run wp-env run cli wp rewrite flush '"--hard"'
+if [ "$TYPE" == "lamp" ]; then
+  $WP_CLI_PREFIX wp rewrite structure /%postname%/ --hard $WP_CLI_SUFFIX
+  $WP_CLI_PREFIX wp rewrite flush --hard $WP_CLI_SUFFIX
+else
+  $WP_CLI_PREFIX wp rewrite structure '"/%postname%/"' '"--hard"' $WP_CLI_SUFFIX
+  $WP_CLI_PREFIX wp rewrite flush '"--hard"' $WP_CLI_SUFFIX
+fi
 
 print_header "Running the cron"
-npm run wp-env run cli wp cron event run '"--due-now"'
+if [ "$TYPE" == "lamp" ]; then
+  $WP_CLI_PREFIX wp cron event run --due-now $WP_CLI_SUFFIX
+else
+  $WP_CLI_PREFIX wp cron event run '"--due-now"' $WP_CLI_SUFFIX
+fi
